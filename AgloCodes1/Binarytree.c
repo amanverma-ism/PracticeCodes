38edb5518471d85a9b50574d4e73830adba44843
Aman Verma <44524984+amanverma-ism@users.noreply.github.com>
2019-01-24 16:52:53 +0530

    Update Binarytree.c

--- AgloCodes1/Binarytree.c
+++ AgloCodes1/Binarytree.c
@@ -14,6 +14,16 @@ typedef enum bool
     true
 } bool;
 
+struct Node* createNode(int key)
+{
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->left = NULL;
+    node->right = NULL;
+    node->data = key;
+    node->parent = NULL;
+    return node;
+}
+
 struct Node* createTree()
 {
     struct Node* root = (struct Node*)malloc(sizeof(struct Node));
@@ -137,8 +147,6 @@ struct Node* treeSuccessor(struct Node* x)
     return y;
 }
 
-
-
 struct Node* treePredecessor(struct Node* x)
 {
     if(x->left!=NULL)
@@ -152,10 +160,125 @@ struct Node* treePredecessor(struct Node* x)
     return y;
 }
 
+void inorder_treeTraversal(struct Node* root)
+{
+    if(root != NULL)
+    {
+        inorder_treeTraversal(root->left);
+        printf("%d ", root->data);
+        inorder_treeTraversal(root->right);
+    }
+}
+
+void preorder_treeTraversal(struct Node* root)
+{
+    if(root != NULL)
+    {
+        printf("%d ", root->data);
+        preorder_treeTraversal(root->left);
+        preorder_treeTraversal(root->right);
+    }
+}
+
+void postorder_treeTraversal(struct Node* root)
+{
+    if(root != NULL)
+    {
+        postorder_treeTraversal(root->left);
+        postorder_treeTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void treeInsert(struct Node** rootref, struct Node* z)
+{
+    struct Node* y = NULL;
+    struct Node* x = (*rootref);
+    while(x!=NULL)
+    {
+        y = x;
+        if(x->data > z->data)
+        {
+            x = x->left;
+        }
+        else
+        {
+            x = x->right;
+        }
+    }
+    z->parent = y;
+    if(y == NULL)
+    {
+        (*rootref) = z;
+    }
+    else if(z->data < y->data)
+        y->left = z;
+    else
+        y->right = z;
+}
+
+void transplant(struct Node**rootref, struct Node* u, struct Node* v)
+{
+    if(u->parent == NULL)
+        (*rootref) = v;
+    else if(u==u->parent->left)
+    {
+        u->parent->left = v;
+    }
+    else
+    {
+        u->parent->right = v;
+    }
+    if(v!=NULL)
+        v->parent = u->parent;
+}
+
+void treeDelete(struct Node** rootref, struct Node* z)
+{
+    if(z->left == NULL)
+        transplant(rootref, z, z->right);
+    else if(z->right == NULL)
+        transplant(rootref, z, z->left);
+    else
+    {
+        struct Node* y = treeMinimum(z->right);
+        if(y->parent != z)
+        {
+            transplant(rootref, y, y->right);
+            y->right = z->right;
+            y->right->parent = y;
+        }
+        transplant(rootref, z, y);
+        y->left = z->left;
+        y->left->parent = y;
+    }
+}
+
+
 int main()
 {
-    struct Node* root = createTree();
-    printf("%d", treeSuccessor(treeSearch(root, 5))->data);
+    struct Node* root = NULL;
+
+    treeInsert(&root, createNode(5));
+    treeInsert(&root, createNode(3));
+    treeInsert(&root, createNode(8));
+    treeInsert(&root, createNode(2));
+    treeInsert(&root, createNode(4));
+    treeInsert(&root, createNode(6));
+    treeInsert(&root, createNode(9));
+    inorder_treeTraversal(root);
+    printf("\n");
+    preorder_treeTraversal(root);
+    printf("\n");
+    postorder_treeTraversal(root);
+    printf("\n");
+    treeDelete(&root, treeSearch(root, 8));
+    inorder_treeTraversal(root);
+    printf("\n");
+    preorder_treeTraversal(root);
+    printf("\n");
+    postorder_treeTraversal(root);
+    printf("\n");
     return 0;
 
 }
